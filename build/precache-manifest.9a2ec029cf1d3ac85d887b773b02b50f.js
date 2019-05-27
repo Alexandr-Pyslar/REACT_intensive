b329a8037bf76e8fe1d9493d7182a45b5a668897
Alexandr-Pyslar <alexandr.pyslar@gmail.com>
2019-05-27 21:15:04 +0300

    Add files via upload

---
+++ build/precache-manifest.9a2ec029cf1d3ac85d887b773b02b50f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1afb3a722bea204b637cfa42dfdab811",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaf4a5cf2cb0aa604b5b",
+    "url": "/static/css/main.fcb48c17.chunk.css"
+  },
+  {
+    "revision": "6724302388bd9ac8cdd4",
+    "url": "/static/js/2.49f87a8f.chunk.js"
+  },
+  {
+    "revision": "eaf4a5cf2cb0aa604b5b",
+    "url": "/static/js/main.94fe5c95.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/static/media/logo.9e8a2b39.svg"
+  }
+]);
